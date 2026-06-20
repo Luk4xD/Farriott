312342513a5d38436a461e66840680e9c0f16c70
Luk4xD <phatnguyenzap12@gmail.com>
2026-06-20 16:27:13 +0700

    feat: initialize project structure with React, Vite, and UI component library

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./global.css";
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      Hello World !
+    </div>
+  );
+}
+
+export default App;
